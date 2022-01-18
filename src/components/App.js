a91c02191681fbf5e483b32cafeaf2e6ff3d73ae
Kim Nahyeong <of.kimna@gmail.com>
2022-01-18 22:07:22 +0900

    search-list 레이아웃
    작고 소중한 레이아웃 첫걸음

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,11 @@
 import './App.scss';
-import logo from 'assets/logo.svg'
+import SearchList from './SearchList/SearchList';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header>header</header>
+      <SearchList/>
     </div>
   );
 }
---
+++ src/components/SearchList/SearchList.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './SearchListStyle.scss'
+
+const SearchList = () =>{
+    return (
+        <>
+            <div className="wrapper">
+                <div className="contents">
+                    <div className="search-result">
+                        
+                    </div>
+                    <div className="search-item">
+                        
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default SearchList;
