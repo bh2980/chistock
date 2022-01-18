d3b4e330cf03bbbfa19a3596bd8f7c9880fcb249
Kim Nahyeong <of.kimna@gmail.com>
2022-01-18 23:05:58 +0900

    feat(SearchList): SearchList layout set

--- src/components/SearchList/SearchList.jsx
+++ src/components/SearchList/SearchList.jsx
@@ -7,11 +7,14 @@ const SearchList = () =>{
             <div className="wrapper">
                 <div className="contents">
                     <div className="search-result">
-                        
+                        <h1>'APPLE' 검색 결과 6건</h1>
                     </div>
                     <div className="search-item">
                         
                     </div>
+                    <div className="search-item"></div>
+                    <div className="search-item"></div>
+                    <div className="search-item"></div>
                 </div>
             </div>
         </>
