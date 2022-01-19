da0ad8a9ccf068cb7e5d6144dfd45c4ef4348b91
kasterra <kasterra@naver.com>
2022-01-19 10:29:24 +0900

    feat(router) : routing to search-list

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "components/Home/Home";
+import SearchList from "components/SearchList/SearchList";
 
 const AppRouter = () => {
   return (
     <Router>
       <Switch>
         <Route path="/" exact component={Home} />
+        <Route path="/search-list" component={SearchList} />
       </Switch>
     </Router>
   );
