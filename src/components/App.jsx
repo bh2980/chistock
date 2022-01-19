d3639940ca10b00bfde293b386e0317f83a09d21
kasterra <kasterra@naver.com>
2022-01-19 10:27:21 +0900

    feat(dev) : router config

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import "./App.scss";
+import AppRouter from "routes/AppRouter";
+
+function App() {
+  return (
+    <div className="App">
+      <header>header</header>
+      <AppRouter />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/routes/AppRouter.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "components/Home/Home";
+
+const AppRouter = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Home} />
+      </Switch>
+    </Router>
+  );
+};
+
+export default AppRouter;
