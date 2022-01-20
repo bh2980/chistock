49653acbbf3a8755a89c3ffa5cc5f48d54e095c5
Kim Nahyeong <of.kimna@gmail.com>
2022-01-20 10:40:58 +0900

    feat(SearchList): Specific Layout - Numbers

--- src/components/SearchList/SearchList.jsx
+++ src/components/SearchList/SearchList.jsx
@@ -6,15 +6,30 @@ const SearchList = () =>{
         <>
             <div className="wrapper">
                 <div className="contents">
-                    <div className="search-result">
+                    <div className="search-result shadow-box">
                         <h1>'APPLE' 검색 결과 6건</h1>
                     </div>
-                    <div className="search-item">
-                        
+                    <div className="search-item shadow-box">
+                        <div className="number shadow-box">
+                            <h1>1</h1>
+                        </div>
+                        <div className="company-name">Apple Inc. (APPL)</div>
                     </div>
-                    <div className="search-item"></div>
-                    <div className="search-item"></div>
-                    <div className="search-item"></div>
+                    <div className="search-item shadow-box">
+                        <div className="number shadow-box">
+                            <h1>2</h1>
+                        </div>
+                    </div>
+                    <div className="search-item shadow-box">
+                        <div className="number shadow-box">
+                            <h1>3</h1>
+                        </div>
+                    </div>
+                    <div className="search-item shadow-box">
+                        <div className="number shadow-box">
+                            <h1>4</h1>
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
