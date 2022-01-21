3bccbba88f35a7f38b3cc827e000822c100eb44d
Hellen <phyy5397@gmial.com>
2022-01-21 19:14:27 +0900

    feat(Header): Header 생성

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Header = () => {
+	return <div className="haeder-wrap">Header Area</div>;
+};
+
+export default Header;
--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,19 +1,21 @@
-import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Home from "components/Home/Home";
-import SearchList from "components/SearchList/SearchList";
-import Company from "components/Company/Company";
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from 'components/Home/Home';
+import SearchList from 'components/SearchList/SearchList';
+import Company from 'components/Company/Company';
+import HeaderContainer from 'components/Header/containers/HeaderContainer';
 
 const AppRouter = () => {
-  return (
-    <Router>
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/search-list" component={SearchList} />
-        <Route path="/company" component={Company} />
-      </Switch>
-    </Router>
-  );
+	return (
+		<Router>
+			<HeaderContainer component={HeaderContainer} />
+			<Switch>
+				<Route path="/" exact component={Home} />
+				<Route path="/search-list" component={SearchList} />
+				<Route path="/company" component={Company} />
+			</Switch>
+		</Router>
+	);
 };
 
 export default AppRouter;
