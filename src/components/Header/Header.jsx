a8f082f9cdc0cc6f5db77b730648c1da74f02aaa
Hellen <phyy5397@gmial.com>
2022-01-21 19:59:45 +0900

    feat(Header): Header 퍼블리싱

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,31 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './css/index.css';
 
 const Header = () => {
-	return <div className="haeder-wrap">Header Area</div>;
+	return (
+		<div className="haeder-wrap">
+			<div className="logo-image">
+				<Link
+					to={{
+						pathname: '/',
+					}}
+				>
+					stock
+				</Link>
+			</div>
+			<div className="input-box shadow-box">
+				<form className="search">
+					<input
+						className="search-input shadow-box"
+						type="text"
+						placeholder="종목명, 종목코드를 입력해 주세요"
+					/>
+				</form>
+				{/* 검색 아이콘 넣어야 함! */}
+			</div>
+		</div>
+	);
 };
 
 export default Header;
