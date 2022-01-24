6e2ebe836ffa90dea3d8e03cb977dd95705f57d9
Kim Nahyeong <of.kimna@gmail.com>
2022-01-24 22:16:57 +0900

    Fix(SearchList) : Code organized via Code Review

--- src/components/SearchList/SearchList.jsx
+++ src/components/SearchList/SearchList.jsx
@@ -1,34 +1,34 @@
 import React from 'react';
 import './styles/SearchListStyle.scss';
 
-const SearchList = () => {
-	const companies = [
-		{
-			name: 'Apple Inc. (APPL)',
-			stockPrice: '172.19',
-			revisedPrice: '-3.34',
-			percent: '-1.41',
-		},
-		{
-			name: 'Maui Land & Pinea... (MLP)',
-			stockPrice: '10.08',
-			revisedPrice: '0.08',
-			percent: '+0.79',
-		},
-		{
-			name: 'Apple Hospitality... (APLE)',
-			stockPrice: '172.19',
-			revisedPrice: '0.12',
-			percent: '+0.72',
-		},
-		{
-			name: 'GCP Applied Tech... (GCP)',
-			stockPrice: '31.87',
-			revisedPrice: '-0.08',
-			percent: '-0.25',
-		},
-	];
+const companies = [
+	{
+		name: 'Apple Inc. (APPL)',
+		stockPrice: '172.19',
+		revisedPrice: '-3.34',
+		percent: '-1.41',
+	},
+	{
+		name: 'Maui Land & Pinea... (MLP)',
+		stockPrice: '10.08',
+		revisedPrice: '0.08',
+		percent: '+0.79',
+	},
+	{
+		name: 'Apple Hospitality... (APLE)',
+		stockPrice: '172.19',
+		revisedPrice: '0.12',
+		percent: '+0.72',
+	},
+	{
+		name: 'GCP Applied Tech... (GCP)',
+		stockPrice: '31.87',
+		revisedPrice: '-0.08',
+		percent: '-0.25',
+	},
+];
 
+const SearchList = () => {
 	const searchList = companies.map((company, index) => (
 		<div className="search-item shadow-box">
 			<div className="number shadow-box">
@@ -59,20 +59,18 @@ const SearchList = () => {
 	));
 
 	return (
-		<>
-			<div className="wrapper">
-				<div className="contents">
-					<div className="search-result shadow-box">
-						<div className="search-num-text">
-							<span>'APPLE' 검색 결과 </span>
-							<span className="search-num">{companies.length}</span>
-							<span> 건</span>
-						</div>
+		<div className="wrapper">
+			<div className="contents">
+				<div className="search-result shadow-box">
+					<div className="search-num-text">
+						<span>'APPLE' 검색 결과 </span>
+						<span className="search-num">{companies.length}</span>
+						<span> 건</span>
 					</div>
-					<div>{searchList}</div>
 				</div>
+				<div>{searchList}</div>
 			</div>
-		</>
+		</div>
 	);
 };
 
