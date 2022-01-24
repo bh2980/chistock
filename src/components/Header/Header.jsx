e79ec55c76c716cbe689f0919b3c2c11d0d117a0
Hellen <phyy5397@gmial.com>
2022-01-24 14:33:31 +0900

    feat(Header): api 달라

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,24 +7,34 @@ import { ReactComponent as ChistockIcon } from 'assets/icons/chistock-icon.svg';
 const Header = () => {
 	return (
 		<div className="haeder-wrap">
-			<div className="logo-image">
-				<Link
-					to={{
-						pathname: '/',
-					}}
-				>
-					<ChistockIcon className="logo" />
-				</Link>
+			<div className="header-top">
+				<div className="logo-image">
+					<Link
+						to={{
+							pathname: '/',
+						}}
+					>
+						<ChistockIcon className="logo" />
+					</Link>
+				</div>
+				<div className="input-box shadow-box">
+					<form className="search">
+						<input
+							className="search-input shadow-box"
+							type="text"
+							placeholder="종목명, 종목코드를 입력해 주세요"
+						/>
+					</form>
+					<SearchIcon className="search-icon" />
+				</div>
 			</div>
-			<div className="input-box shadow-box">
-				<form className="search">
-					<input
-						className="search-input shadow-box"
-						type="text"
-						placeholder="종목명, 종목코드를 입력해 주세요"
-					/>
-				</form>
-				<SearchIcon className="search-icon" />
+			<div className="header-bottom">
+				<div className="banner-area">
+					<marquee scroll>
+						FB 333.26 <span className="red-text">-1.1 -0.33%</span>
+						FB 333.26 <span className="red-text">-1.1 -0.33%</span>
+					</marquee>
+				</div>
 			</div>
 		</div>
 	);
