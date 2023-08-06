5a3a37a3f0fd971e09f49d5e2ff383d7252542c5
bh2980 <bh2980@naver.com>
2023-08-06 20:21:46 +0900

    feat: 아이콘 컴포넌트 추가

---
+++ src/constants/iconMap.ts
@@ -0,0 +1,14 @@
+const ICON_MAP = {
+  Add: () => import("@assets/icons/add.svg"),
+  ArrowBack: () => import("@assets/icons/arrowBack.svg"),
+  DarkMode: () => import("@assets/icons/darkMode.svg"),
+  Error: () => import("@assets/icons/error.svg"),
+  LightMode: () => import("@assets/icons/lightMode.svg"),
+  Loading: () => import("@assets/icons/loading.svg"),
+  Refresh: () => import("@assets/icons/refresh.svg"),
+  Search: () => import("@assets/icons/search.svg"),
+  Settings: () => import("@assets/icons/settings.svg"),
+  SyncProblem: () => import("@assets/icons/syncProblem.svg"),
+};
+
+export default ICON_MAP;
