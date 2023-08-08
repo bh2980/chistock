e58b725aa1c086dec59a4d9430b18720b0b69f9a
bh2980 <bh2980@naver.com>
2023-08-08 15:59:30 +0900

    feat: Divider 컴포넌트 추가

---
+++ src/components/atoms/Divider/Divider.tsx
@@ -0,0 +1,34 @@
+import { ComponentProps } from "react";
+import { twJoin, twMerge } from "tailwind-merge";
+
+type DividerPropsType = {
+  color: "outline" | "outlineVariant";
+  direction: "horizontal" | "vertical";
+  thickness: "s" | "m";
+} & ComponentProps<"div">;
+
+const Divider = ({
+  className,
+  direction,
+  thickness,
+  color,
+}: DividerPropsType) => {
+  return (
+    <div
+      className={twMerge(
+        className,
+        twJoin(
+          color === "outline" ? "border-outline" : "border-outline-variant",
+          direction === "horizontal" && [
+            thickness === "s" ? "border-t-s" : "border-t-m",
+          ],
+          direction === "vertical" && [
+            thickness === "s" ? "border-l-s" : "border-l-m",
+          ]
+        )
+      )}
+    />
+  );
+};
+
+export default Divider;
