f8de620775a99f0aff63ba2c32a13f3f7eb2998a
bh2980 <bh2980@naver.com>
2023-08-14 15:34:05 +0900

    feat: PolymorphicComponentType 추가

--- src/customTypes/polymorphicType.ts
+++ src/customTypes/polymorphicType.ts
@@ -2,14 +2,16 @@ type AsPropType<T extends React.ElementType> = {
   as?: T;
 };
 
-export type PolymorphicRefType<T extends React.ElementType> =
-  React.ComponentPropsWithRef<T>["ref"];
+export type PolymorphicRefType<T extends React.ElementType> = React.ComponentPropsWithRef<T>["ref"];
 
-export type PolymorphicPropsType<
-  T extends React.ElementType,
-  Props = object
-> = AsPropType<T> &
+export type PolymorphicPropsType<T extends React.ElementType, Props = object> = AsPropType<T> &
   Props &
   Omit<React.ComponentPropsWithoutRef<T>, keyof Props> & {
     ref?: PolymorphicRefType<T>;
   };
+
+export type PolymorphicComponentType<TDefault extends React.ElementType, K = object> = <
+  T extends React.ElementType = TDefault
+>(
+  props: PolymorphicPropsType<T, K>
+) => React.ReactNode | null;
