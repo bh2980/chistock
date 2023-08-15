d47951cd510f10ad27aa89818925c536b0b3b420
bh2980 <bh2980@naver.com>
2023-08-15 12:13:22 +0900

    refactor: 커스텀 twMerge 적용

--- src/utils/classMerge.ts
+++ src/utils/classMerge.ts
@@ -1,13 +1,10 @@
 import { cx } from "class-variance-authority";
-import { twMerge } from "tailwind-merge";
+import { ClassNameValue } from "tailwind-merge";
 
-const classMerge = (classes: string | (string | undefined)[]) => {
-  const fontSizePattern =
-    /(text-(xs|s|m|l|xl|2xl))[ ]+|(text-(xs|s|m|l|xl|2xl)$)/g;
+import twMerge from "./customTwMerge";
 
-  return `${twMerge(cx(classes).replace(fontSizePattern, ""))} ${twMerge(
-    cx(classes).match(fontSizePattern)
-  )}`;
+const classMerge = (classes: ClassNameValue[]) => {
+  return twMerge(cx(classes));
 };
 
 export default classMerge;
