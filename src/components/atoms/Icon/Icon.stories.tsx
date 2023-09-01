3823a8ff6feca3a5e760c87ae6db6efd44113031
Janghun Lee <74360958+bh2980@users.noreply.github.com>
2023-09-01 18:59:10 +0900

    Icon 컴포넌트 문서 정리 (#110)
    * feat: ✨ textColor를 constant로 분리
    
    * refactor: ♻️ textColor constnat 적용
    
    * fix: 🐛 textColor에서 tailwind 색상이 불러와지지 않는 문제 fix
    
    * docs: 📝 Icon Story 작성
    
    * docs: 📝 Icon 문서 작성

--- src/components/atoms/Icon/Icon.stories.tsx
+++ src/components/atoms/Icon/Icon.stories.tsx
@@ -1,50 +1,14 @@
 import type { Meta, StoryObj } from "@storybook/react";
+
+import StoryWrapper from "@story/StoryWrapper";
+
 import Icon from "./Icon";
-import ICON_MAP from "@constants/iconMap";
 
 const meta = {
   title: "Atom/Icon",
   component: Icon,
   parameters: {
     layout: "centered",
-  },
-  tags: ["autodocs"],
-  argTypes: {
-    icon: {
-      options: Object.keys(ICON_MAP),
-      control: { type: "radio" },
-    },
-    color: {
-      options: [
-        "current",
-        "primary",
-        "primaryFixed",
-        "secondary",
-        "secondaryFixed",
-        "tertiary",
-        "tertiaryFixed",
-        "red",
-        "yellow",
-        "green",
-        "magenta",
-        "onSurface",
-        "onSub",
-        "onPrimary",
-        "onPrimaryFixed",
-        "onSecondary",
-        "onSecondaryFixed",
-        "onTertiary",
-        "onTertiaryFixed",
-        "onRed",
-        "onRedSub",
-        "onYellow",
-      ],
-      control: { type: "radio" },
-    },
-    size: {
-      options: ["inherit", "s", "m", "l", "xl", "2xl", "3xl"],
-      control: { type: "radio" },
-    },
   },
 } satisfies Meta<typeof Icon>;
 
@@ -53,12 +17,125 @@ type Story = StoryObj<typeof Icon>;
 
 export const Default: Story = {
   args: {
-    icon: "arrow-path",
-    color: "current",
-    size: "inherit",
+    icon: "sun",
   },
   render: (args) => (
-    <div className="text-[#f58f00] text-[120rem]">
+    <div className="text-[#7b4bff] text-[56rem]">
+      <Icon {...args} />
+    </div>
+  ),
+};
+
+/** Icon 종류를 설정합니다. */
+export const IconCategory: Story = {
+  render: () => (
+    <StoryWrapper className="text-[#7b4bff] text-[56rem]" direction="vertical">
+      <StoryWrapper>
+        <Icon icon="arrow-path" />
+        <Icon icon="chevron-down" />
+        <Icon icon="chevron-up" />
+        <Icon icon="exclamation-circle" />
+      </StoryWrapper>
+      <StoryWrapper>
+        <Icon icon="moon" />
+        <Icon icon="sun" />
+        <Icon icon="search" />
+        <Icon icon="setting" />
+      </StoryWrapper>
+    </StoryWrapper>
+  ),
+};
+
+/**
+ * Icon은 아래 6가지의 크기를 가집니다.
+ *
+ * - `inherit` : 부모 컴포넌트의 text 크기를 상속받습니다.
+ * - `s` : 12px
+ * - `m` : 16px
+ * - `l` : 20px
+ * - `xl` : 24px
+ * - `2xl` : 32px
+ * - `3xl` : 40px
+ *
+ * 기본값은 `inherit`으로 지정되어 있습니다.
+ */
+export const IconSize: Story = {
+  render: () => (
+    <StoryWrapper className="text-[#7b4bff] text-[56rem]" direction="vertical">
+      <Icon icon="sun" />
+      <StoryWrapper>
+        <Icon icon="sun" size="s" />
+        <Icon icon="sun" size="m" />
+        <Icon icon="sun" size="l" />
+        <Icon icon="sun" size="xl" />
+        <Icon icon="sun" size="2xl" />
+        <Icon icon="sun" size="3xl" />
+      </StoryWrapper>
+    </StoryWrapper>
+  ),
+};
+
+/**
+ * Icon은 색상 26가지의 색상을 가지며, 이는 Text 컴포넌트의 색상과 동일합니다.
+ *
+ * - `inherit`: 부모 컴포넌트의 text 색상을 상속받습니다.
+ * - `transparent`: 부모 컴포넌트의 text 색상을 상속받습니다.
+ *
+ *
+ * - `onSurface` : surface 위 컨텐츠 색
+ * - `onSurfaceVariant` : surface 위 컨텐츠 색 2
+ *
+ *
+ * - `유채색`
+ * - `유채색 위 컨텐츠 색(On)`
+ */
+export const IconColor: Story = {
+  render: () => (
+    <StoryWrapper className="text-[#7b4bff] text-[56rem]" direction="vertical">
+      <StoryWrapper>
+        <Icon icon="sun" />
+        <Icon icon="sun" color="transparent" />
+      </StoryWrapper>
+      <StoryWrapper>
+        <Icon icon="sun" className="bg-surface rounded-circle" color="onSurface" />
+        <Icon icon="sun" className="bg-surface rounded-circle" color="onSurfaceVariant" />
+      </StoryWrapper>
+      <StoryWrapper>
+        <Icon icon="sun" color="primary" />
+        <Icon icon="sun" color="primaryFixed" />
+        <Icon icon="sun" color="secondary" />
+        <Icon icon="sun" color="secondaryFixed" />
+        <Icon icon="sun" color="tertiary" />
+        <Icon icon="sun" color="tertiaryFixed" />
+        <Icon icon="sun" color="red" />
+        <Icon icon="sun" color="redVariant" />
+        <Icon icon="sun" color="yellow" />
+        <Icon icon="sun" color="green" />
+        <Icon icon="sun" color="magenta" />
+      </StoryWrapper>
+      <StoryWrapper>
+        <Icon icon="sun" className="bg-primary rounded-circle" color="onPrimary" />
+        <Icon icon="sun" className="bg-primary-fixed rounded-circle" color="onPrimaryFixed" />
+        <Icon icon="sun" className="bg-secondary rounded-circle" color="onSecondary" />
+        <Icon icon="sun" className="bg-secondary-fixed rounded-circle" color="onSecondaryFixed" />
+        <Icon icon="sun" className="bg-tertiary rounded-circle" color="onTertiary" />
+        <Icon icon="sun" className="bg-tertiary-fixed rounded-circle" color="onTertiaryFixed" />
+        <Icon icon="sun" className="bg-red rounded-circle" color="onRed" />
+        <Icon icon="sun" className="bg-red-variant rounded-circle" color="onRedVariant" />
+        <Icon icon="sun" className="bg-yellow rounded-circle" color="onYellow" />
+        <Icon icon="sun" className="bg-green rounded-circle" color="onGreen" />
+        <Icon icon="sun" className="bg-magenta rounded-circle" color="onMagenta" />
+      </StoryWrapper>
+    </StoryWrapper>
+  ),
+};
+
+export const Playground: Story = {
+  args: {
+    icon: "sun",
+  },
+  render: (args) => (
+    <div className="text-[#7b4bff] text-[56rem]">
       <Icon {...args} />
     </div>
   ),
