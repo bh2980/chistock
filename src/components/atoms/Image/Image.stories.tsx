57d3b601c1bc21b7dc35b52f37c15a9d0df294f6
Janghun Lee <74360958+bh2980@users.noreply.github.com>
2023-09-01 20:24:21 +0900

    Image 컴포넌트 문서 작성 (#111)
    * feat: ✨ Image 컴포넌트 types 별도 파일로 분리
    
    * docs: 📝 Image Story 작성
    
    * docs: 📝 Image mdx 문서 추가

--- src/components/atoms/Image/Image.stories.tsx
+++ src/components/atoms/Image/Image.stories.tsx
@@ -1,5 +1,7 @@
 import type { Meta, StoryObj } from "@storybook/react";
 
+import StoryWrapper from "@story/StoryWrapper";
+
 import Image from "./Image";
 
 const meta = {
@@ -8,17 +10,73 @@ const meta = {
   parameters: {
     layout: "centered",
   },
-  tags: ["autodocs"],
+  // tags: ["autodocs"],
   argTypes: {
-    rounded: {
-      options: ["none", "xs", "s", "m", "l", "circle"],
-      control: { type: "radio" },
+    loading: {
+      table: {
+        disable: true,
+      },
     },
-  },
-  args: {
-    className: "h-[160rem]",
-    src: "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FbixMVD%2FbtsnzFZlqlV%2FJiuZ2EpykdTd37Kljphibk%2Fimg.png",
-    alt: "react component",
+    loader: {
+      table: {
+        disable: true,
+      },
+    },
+    quality: {
+      table: {
+        disable: true,
+      },
+    },
+    priority: {
+      table: {
+        disable: true,
+      },
+    },
+    blurDataURL: {
+      table: {
+        disable: true,
+      },
+    },
+    unoptimized: {
+      table: {
+        disable: true,
+      },
+    },
+    onLoadingComplete: {
+      table: {
+        disable: true,
+      },
+    },
+    layout: {
+      table: {
+        disable: true,
+      },
+    },
+    objectFit: {
+      table: {
+        disable: true,
+      },
+    },
+    objectPosition: {
+      table: {
+        disable: true,
+      },
+    },
+    lazyBoundary: {
+      table: {
+        disable: true,
+      },
+    },
+    lazyRoot: {
+      table: {
+        disable: true,
+      },
+    },
+    fill: {
+      table: {
+        disable: true,
+      },
+    },
   },
 } satisfies Meta<typeof Image>;
 
@@ -26,5 +84,75 @@ export default meta;
 type Story = StoryObj<typeof Image>;
 
 export const Default: Story = {
+  render: () => <Image width="w-[200rem]" alt="chistock 이미지" src="https://url.kr/kd35ap" />,
+};
+
+/**
+ * Image는 width와 height를 통해 크기를 지정할 수 있습니다.
+ *
+ * - `width`: 필수 속성으로 width만 지정 시 기본적으로 1:1의 비율로 렌더링됩니다.
+ * - `height`: 선택 속성으로 필요 시 지정합니다.
+ */
+export const ImageSize: Story = {
+  render: () => (
+    <StoryWrapper>
+      <Image width="w-[100rem]" alt="chistock 이미지" src="https://url.kr/kd35ap" />
+      <Image
+        width="w-[200rem]"
+        height="h-[100rem]"
+        alt="chistock 이미지"
+        src="https://url.kr/kd35ap"
+      />
+    </StoryWrapper>
+  ),
+};
+
+/**
+ * Image는 rounded 속성을 통해 테두리 반경을 조절할 수 있습니다.
+ *
+ * - `none` : 없음
+ * - `xs` : 2px
+ * - `s` : 4px
+ * - `m` : 8px
+ * - `l` : 16px
+ * - `circle` : 둥근 테두리
+ */
+export const ImageRadius: Story = {
+  render: () => (
+    <StoryWrapper>
+      <Image rounded="none" width="w-[100rem]" alt="chistock 이미지" src="https://url.kr/kd35ap" />
+      <Image rounded="xs" width="w-[100rem]" alt="chistock 이미지" src="https://url.kr/kd35ap" />
+      <Image rounded="s" width="w-[100rem]" alt="chistock 이미지" src="https://url.kr/kd35ap" />
+      <Image rounded="m" width="w-[100rem]" alt="chistock 이미지" src="https://url.kr/kd35ap" />
+      <Image rounded="l" width="w-[100rem]" alt="chistock 이미지" src="https://url.kr/kd35ap" />
+      <Image
+        rounded="circle"
+        width="w-[100rem]"
+        alt="chistock 이미지"
+        src="https://url.kr/kd35ap"
+      />
+    </StoryWrapper>
+  ),
+};
+
+export const Playground: Story = {
+  argTypes: {
+    width: {
+      options: ["w-[200rem]", "w-[400rem]", "w-[800rem]"],
+      control: { type: "select" },
+    },
+    height: {
+      options: ["h-[200rem]", "h-[400rem]", "h-[800rem]"],
+      control: { type: "select" },
+    },
+    src: {
+      control: { type: "text" },
+    },
+  },
+  args: {
+    width: "w-[200rem]",
+    src: "https://url.kr/kd35ap",
+    alt: "chistock",
+  },
   render: (args) => <Image {...args} />,
 };
--- src/components/atoms/Image/Image.tsx
+++ src/components/atoms/Image/Image.tsx
@@ -1,10 +1,16 @@
+import { cva } from "class-variance-authority";
 import * as NextImage from "next/image";
-import { VariantProps, cva } from "class-variance-authority";
 
 import classMerge from "@utils/classMerge";
 
-const ImageVariants = cva("relative min-w-[1em] overflow-hidden aspect-square", {
+import { ImagePropsType } from "./Image.types";
+
+export const ImageVariants = cva("relative min-w-[1em] overflow-hidden aspect-square", {
   variants: {
+    /** 이미지의 테두리 반경을 조절합니다
+     *
+     * @default m
+     */
     rounded: {
       none: "rounded-none",
       xs: "rounded-xs",
@@ -19,24 +25,27 @@ const ImageVariants = cva("relative min-w-[1em] overflow-hidden aspect-square", 
   },
 });
 
-type ImagePropsType = Omit<
-  NextImage.ImageProps,
-  | "width"
-  | "height"
-  | "fill"
-  //이하 deprecated props
-  | "layout"
-  | "objectFit"
-  | "objectPosition"
-  | "lazyBoundary"
-  | "lazyRoot"
-> &
-  VariantProps<typeof ImageVariants>;
-
-const Image = ({ className, rounded, alt, ...props }: ImagePropsType) => {
+/**
+ * next/image를 사용하여 이미지를 렌더링하는 컴포넌트입니다.
+ *
+ * 기본적으로 컨테이너 내에 비율을 유지한채로 꽉 채워 렌더링되며 컨테이너를 넘어가는 부분은 잘리게 됩니다.
+ *
+ * next/image의 prop 역시 사용 가능합니다.
+ *
+ * @todo 정적 이미지일 경우 width, height를 받을 필요 없도록
+ */
+const Image = ({
+  className,
+  width,
+  height,
+  rounded,
+  alt,
+  fill = true,
+  ...props
+}: ImagePropsType) => {
   return (
-    <div className={classMerge([className, ImageVariants({ rounded })])}>
-      <NextImage.default fill={true} alt={alt} style={{ objectFit: "cover" }} {...props} />
+    <div className={classMerge([ImageVariants({ rounded }), width, height, className])}>
+      <NextImage.default style={{ objectFit: "cover" }} alt={alt} fill={fill} {...props} />
     </div>
   );
 };
