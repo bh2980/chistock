2eb2ab22acc905a6f3458ea4c288508136b2b1c3
bh2980 <bh2980@naver.com>
2023-09-01 15:27:43 +0900

    refactor: ♻️ 구별된 Union을 활용하여 Divider 리팩토링
    type을 별도 파일로 분리
    
    ✅ Closes: #106

---
+++ src/components/atoms/Divider/Divider.types.ts
@@ -0,0 +1,46 @@
+import { TailwindHeightClassType, TailwindWidthClassType } from "@customTypes/tailwindFormatType";
+
+/** 가로 방향 Divider가 가지는 속성 타입 */
+type HorizontalDividerType = {
+  /** Divider의 방향을 결정하는 속성
+   *
+   * @default 'horizontal'
+   */
+  direction?: "horizontal";
+  /** Divider의 길이를 결정하는 속성 */
+  length: TailwindWidthClassType;
+};
+
+/** 세로 방향 Divider가 가지는 속성 타입 */
+type VerticalDividerType = {
+  direction: "vertical";
+  length: TailwindHeightClassType;
+};
+
+/** 방향에 따른 Divider 속성 타입 */
+type DividerDirectionType = HorizontalDividerType | VerticalDividerType;
+
+/** Divider이 가지는 공통 속성 타입*/
+type DividerBaseType = {
+  /**
+   * Divider의 색상을 결정하는 속성
+   *  @default 'outline'
+   */
+  color?: "outline" | "outlineVariant";
+  /**
+   * Divider의 굵기를 결정하는 속성
+   * @default 'm'
+   */
+  thickness?: "s" | "m";
+};
+
+/**
+ * Divider에 들어가는 Props 타입
+ *
+ * direction에 따라서 HorizontalDividerType 혹은 VerticalDividerType이 포함됩니다.
+ *
+ * @example
+ * direction = 'horizontal' : HorizontalDividerType & DividerBaseType & divProps
+ * direction = 'vertical' : VerticalDividerType & DividerBaseType & divProps
+ *  */
+export type DividerPropsType = DividerDirectionType & DividerBaseType & React.ComponentProps<"div">;
