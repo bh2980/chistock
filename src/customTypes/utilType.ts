313e26b9252285c3449fda9471e17c8188bbdd16
bh2980 <bh2980@naver.com>
2023-09-04 00:13:47 +0900

    feat: ✨ VariantPropsType 추가

--- src/customTypes/utilType.ts
+++ src/customTypes/utilType.ts
@@ -1,3 +1,12 @@
-export type NonNullableProps<Props> = {
-  [P in keyof Props]: NonNullable<Props[P]>;
+import { VariantProps } from "class-variance-authority";
+
+/** 객체 속성의 값들에서 Nullable한 값 제거하는 타입 */
+export type NonNullableProps<Obj> = {
+  [P in keyof Obj]: NonNullable<Obj[P]>;
 };
+
+/** VariantsProps의 속성의 값들에서 Nullable한 값 제거한 타입 */
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type VariantPropsType<Component extends (...args: any) => any> = NonNullableProps<
+  VariantProps<Component>
+>;
