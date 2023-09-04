9195af931e3a0e2ac901322c4061d8c742c96f10
bh2980 <bh2980@naver.com>
2023-09-05 00:21:47 +0900

    feat: ✨ exceptProperty 함수 추가 및 utils 함수 파일 통합

---
+++ src/utils/utils.ts
@@ -0,0 +1,46 @@
+import { cx } from "class-variance-authority";
+import { extendTailwindMerge } from "tailwind-merge";
+import { ClassNameValue } from "tailwind-merge";
+import resolveConfig from "tailwindcss/resolveConfig";
+
+import tailwindConfig from "@root/tailwind.config";
+
+const customTailwindTheme = resolveConfig(tailwindConfig).theme;
+
+/** Tailwind 설정으로부터 커스텀 토큰 리스트를 가져오는 함수 */
+const getToken = (tokenName: Exclude<keyof typeof customTailwindTheme, "extend">) =>
+  Object.keys(customTailwindTheme[tokenName]).filter((key) => key !== "DEFAULT");
+
+/** 커스텀 twMerge */
+const twMerge = extendTailwindMerge({
+  theme: {
+    colors: getToken("colors"),
+    spacing: getToken("spacing"),
+    borderRadius: getToken("borderRadius"),
+    borderWidth: getToken("borderWidth"),
+  },
+  classGroups: {
+    "font-size": [{ text: getToken("fontSize") }],
+  },
+});
+
+/** cx와 twMerge로 tailwind class를 충돌을 방지하면서 합치는 함수 */
+export const classMerge = (classes: ClassNameValue | ClassNameValue[]) => {
+  return twMerge(cx(classes));
+};
+
+type AccType = { [key: string]: { table: { disable: true } } };
+
+/** Storybook에서 ArgsTable에서 제외할 타입들을 argsType 객체로 만드는 함수 */
+export const exceptProperty = (exceptProperties: string[]) => {
+  return exceptProperties.reduce((acc: AccType, cur) => {
+    console.log(acc, cur);
+    acc[cur] = {
+      table: {
+        disable: true,
+      },
+    };
+
+    return acc;
+  }, {});
+};
