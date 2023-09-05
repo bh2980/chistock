e3a0918502badb3fd62443644085f19881d61869
Janghun Lee <74360958+bh2980@users.noreply.github.com>
2023-09-05 21:03:32 +0900

    fix: 🐛 createBox 삭제 -> 렌더링 이슈 수정 (#119)

--- src/components/atoms/Box/Box.tsx
+++ src/components/atoms/Box/Box.tsx
@@ -1,23 +1,5 @@
 import { PolymorphicPropsWithInnerRefType } from "@customTypes/polymorphicType";
 
-/** renderAs의 기본값이 지정된 Box를 반환하는 함수입니다.
- *
- * renderAs의 종류를 제한할 때에는 가능한 타입을 Union으로 결합하여 제네릭으로 넣어주세요.
- *
- * @param `defaultAs` : 기본적으로 렌더링 될 태그 혹은 컴포넌트 타입
- *
- * @example
- * // 기본적으로 `button`태그로 렌더링되면서
- * //`button`, `a` 태그만 렌더링로만 가능한 Box 컴포넌트
- * createBox<'button' | 'a'>('button')
- */
-export const createBox = <T extends React.ElementType>(defaultAs: T) => {
-  // eslint-disable-next-line react/display-name
-  return ({ renderAs = defaultAs, ...rest }: PolymorphicPropsWithInnerRefType<T>) => {
-    return <Box renderAs={renderAs} {...(rest as PolymorphicPropsWithInnerRefType<T>)} />;
-  };
-};
-
 /**
  * 무엇이든 될 수 있는 기본 다형성 컴포넌트입니다.
  *
