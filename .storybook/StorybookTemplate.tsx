bb4375526bb43ff675c94e83a7884200ea65c1f4
bh2980 <bh2980@naver.com>
2023-09-05 12:22:52 +0900

    docs: 📝 Storybook 템플릿 추가

---
+++ .storybook/StorybookTemplate.tsx
@@ -0,0 +1,22 @@
+import { ArgTypes, Description, Primary, Stories, Title } from "@storybook/blocks";
+import React from "react";
+
+const StorybookTemplate = () => {
+  return (
+    <>
+      <br />
+      <br />
+      <br />
+      <br />
+      <Title />
+      <Description />
+      <Primary />
+      <ArgTypes />
+      <Stories />
+      <br />
+      <br />
+    </>
+  );
+};
+
+export default StorybookTemplate;
