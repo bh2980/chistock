ccc9cc3d1a08419cc753cef1761f322635a488a7
bh2980 <bh2980@naver.com>
2024-01-24 00:03:17 +0900

    feat: ✨ tv에 커스텀 twMerge 설정 적용

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,6 +1,7 @@
 import clsx from "clsx";
 import { extendTailwindMerge } from "tailwind-merge";
 import { ClassNameValue } from "tailwind-merge";
+import { createTV } from "tailwind-variants";
 import resolveConfig from "tailwindcss/resolveConfig";
 
 import tailwindConfig from "@root/tailwind.config";
@@ -10,6 +11,21 @@ const customTailwindTheme = resolveConfig(tailwindConfig).theme;
 /** Tailwind 설정으로부터 커스텀 토큰 리스트를 가져오는 함수 */
 const getToken = (tokenName: Exclude<keyof typeof customTailwindTheme, "extend">) =>
   Object.keys(customTailwindTheme[tokenName]).filter((key) => key !== "DEFAULT");
+
+/** tv에 twMerge 설정 적용 */
+export const tv = createTV({
+  twMergeConfig: {
+    theme: {
+      colors: getToken("colors"),
+      spacing: getToken("spacing"),
+      borderRadius: getToken("borderRadius"),
+      borderWidth: getToken("borderWidth"),
+    },
+    classGroups: {
+      "font-size": [{ text: getToken("fontSize") }],
+    },
+  },
+});
 
 /** 커스텀 twMerge */
 const twMerge = extendTailwindMerge({
