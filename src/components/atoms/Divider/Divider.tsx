68f700a6566abb88e1936696c3b442018cb7d570
bh2980 <bh2980@naver.com>
2024-01-23 22:41:05 +0900

    refactor: ♻️ divider 변경

--- src/components/atoms/Divider/Divider.tsx
+++ src/components/atoms/Divider/Divider.tsx
@@ -1,8 +1,50 @@
-import { twJoin, twMerge } from "tailwind-merge";
-
-import { classMerge, makeNum2Unit } from "@utils/utils";
+import { makeNum2Unit, tv } from "@utils/utils";
 
 import { DividerPropsType } from "./Divider.types";
+
+const dividerVariants = tv({
+  variants: {
+    color: {
+      outline: "border-outline",
+      outlineVariant: "border-outline-variant",
+    },
+    direction: {
+      horizontal: "h-[0rem]",
+      vertical: "w-[0rem]",
+    },
+    thickness: {
+      s: "",
+      m: "",
+    },
+  },
+  compoundVariants: [
+    {
+      direction: "horizontal",
+      thickness: "s",
+      className: "border-t-s",
+    },
+    {
+      direction: "horizontal",
+      thickness: "m",
+      className: "border-t-m",
+    },
+    {
+      direction: "vertical",
+      thickness: "s",
+      className: "border-l-s",
+    },
+    {
+      direction: "vertical",
+      thickness: "m",
+      className: "border-l-m",
+    },
+  ],
+  defaultVariants: {
+    color: "outline",
+    direction: "horizontal",
+    thickness: "m",
+  },
+});
 
 /**
  * 영역을 구분하기 위한 구분선을 표시하는 컴포넌트입니다.
@@ -12,25 +54,16 @@ import { DividerPropsType } from "./Divider.types";
 const Divider = ({
   className,
   length,
-  direction = "horizontal",
-  thickness = "m",
-  color = "outline",
+  direction,
+  thickness,
+  color,
   ...props
 }: DividerPropsType) => {
   return (
     <div
-      className={twMerge(
-        twJoin(
-          color === "outline" ? "border-outline" : "border-outline-variant",
-          direction === "horizontal" &&
-            classMerge(["h-[0rem]", [thickness === "s" ? "border-t-s" : "border-t-m"]]),
-          direction === "vertical" &&
-            classMerge(["w-[0rem]", [thickness === "s" ? "border-l-s" : "border-l-m"]])
-        ),
-        className
-      )}
+      className={dividerVariants({ color, thickness, direction, className })}
       style={
-        direction === "horizontal"
+        direction === "horizontal" || direction === undefined
           ? { width: makeNum2Unit(length) }
           : { height: makeNum2Unit(length) }
       }
