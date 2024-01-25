520a450998008c7097d932de946812bf64cd30a9
bh2980 <bh2980@naver.com>
2024-01-25 09:58:51 +0900

    test: ✅ 버튼 a 태그 렌더링 테스트 코드 작성

--- src/components/atoms/Button/Button/Button.test.tsx
+++ src/components/atoms/Button/Button/Button.test.tsx
@@ -91,4 +91,108 @@ describe("Button", () => {
       expect(onClick).not.toHaveBeenCalled();
     });
   });
+
+  describe("a 태그로 렌더링", () => {
+    it("정상 렌더링", () => {
+      render(<Button renderAs="a">Button</Button>);
+      const button = screen.getByRole("button");
+
+      expect(button.tagName).toBe("A");
+      expect(button).toHaveTextContent("Button");
+    });
+
+    it("focus 가능", async () => {
+      render(<Button renderAs="a">Button</Button>);
+      const button = screen.getByRole("button");
+
+      expect(button).not.toHaveFocus();
+      await userEvent.tab();
+
+      expect(button).toHaveFocus();
+    });
+
+    // focus 후 space 시 함수 호출 -> focus 유지
+    it("space 키로 함수 실행", async () => {
+      const onClick = jest.fn();
+
+      render(
+        <Button renderAs="a" onClick={onClick}>
+          Button
+        </Button>
+      );
+      const button = screen.getByRole("button");
+
+      await userEvent.tab();
+      expect(button).toHaveFocus();
+      await userEvent.type(button, "{space}");
+
+      expect(onClick).toHaveBeenCalled();
+      expect(button).toHaveFocus();
+    });
+
+    // enter 시 함수 호출 -> focus 유지
+    it("enter 키로 함수 실행", async () => {
+      const onClick = jest.fn();
+
+      render(
+        <Button renderAs="a" onClick={onClick}>
+          Button
+        </Button>
+      );
+      const button = screen.getByRole("button");
+
+      await userEvent.tab();
+      expect(button).toHaveFocus();
+      await userEvent.type(button, "{enter}");
+
+      expect(onClick).toHaveBeenCalled();
+      expect(button).toHaveFocus();
+    });
+
+    // click 시 함수 호출 -> focus 유지
+    it("click 시 함수 실행", async () => {
+      const onClick = jest.fn();
+
+      render(
+        <Button renderAs="a" onClick={onClick}>
+          Button
+        </Button>
+      );
+      const button = screen.getByRole("button");
+
+      await userEvent.click(button);
+
+      expect(onClick).toHaveBeenCalled();
+      expect(button).toHaveFocus();
+    });
+
+    it("disabled 시 focus 불가", async () => {
+      render(
+        <Button renderAs="a" disabled>
+          Button
+        </Button>
+      );
+      const button = screen.getByRole("button");
+
+      expect(button).not.toHaveFocus();
+      await userEvent.tab();
+
+      expect(button).not.toHaveFocus();
+    });
+
+    it("disabled 시 클릭 불가", async () => {
+      const onClick = jest.fn();
+
+      render(
+        <Button renderAs="a" onClick={onClick} disabled>
+          Button
+        </Button>
+      );
+      const button = screen.getByRole("button");
+
+      await userEvent.click(button);
+
+      expect(onClick).not.toHaveBeenCalled();
+    });
+  });
 });
