a8b67f6ebe8041110edd06faa2e6110079032873
bh2980 <bh2980@naver.com>
2024-01-25 21:39:05 +0900

    refactor: ♻️ FAB에 IconButton 대응

--- src/components/atoms/Button/FloatingButton/FloatingButton.tsx
+++ src/components/atoms/Button/FloatingButton/FloatingButton.tsx
@@ -5,17 +5,17 @@ import { tv } from "@utils/utils";
 import Button from "../Button/Button";
 import { ButtonBasePropsType } from "../Button/Button.types";
 
-type FloatingButtonProps = Omit<
-  PolymorphicPropsType<"button", ButtonBasePropsType>,
-  "variant" | "renderAs"
->;
+type FloatingButtonProps = PolymorphicPropsType<"button", ButtonBasePropsType> & {
+  variant?: "primary" | "text";
+  renderAs?: "button";
+};
 
 const floatingButtonVariant = tv({
-  base: "absolute bg-surface-variant text-surface-on rounded-circle border border-outline-variant overflow-hidden shadow-xl",
+  base: "absolute rounded-circle border border-outline-variant overflow-hidden shadow-xl",
 });
 
-const FloatingButton = ({ className, ...props }: FloatingButtonProps) => {
-  return <Button variant="text" className={floatingButtonVariant({ className })} {...props} />;
+const FloatingButton = ({ className, variant = "text", ...props }: FloatingButtonProps) => {
+  return <Button variant={variant} className={floatingButtonVariant({ className })} {...props} />;
 };
 
 export default FloatingButton;
