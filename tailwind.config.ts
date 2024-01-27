9233892b4b9e380340e783001e6f2bc7ceb73fc5
bh2980 <bh2980@naver.com>
2024-01-28 01:08:58 +0900

    design: 💄 focus를 focus-within으로 변경

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -73,7 +73,7 @@ export default {
       // //InteractionState
       addVariant("interaction", "& .interactionState");
       addVariant("interactionHover", "&:hover .interactionState");
-      addVariant("interactionFocus", "&:focus .interactionState");
+      addVariant("interactionFocus", "&:focus-within .interactionState");
       addVariant("interactionFocusVisible", "&:focus-visible .interactionState");
       addVariant("interactionPress", "&:active .interactionState");
       // hover보다 press가 뒤에 있어야 함. -> CSS 파일 내에서 뒤에 나오는 규칙이 우선 적용
