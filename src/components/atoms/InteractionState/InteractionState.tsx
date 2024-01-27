2620a9bd8b7f1ef449440be6cb6757489cc5c505
bh2980 <bh2980@naver.com>
2024-01-28 00:19:33 +0900

    feat: ✨ InteractioState 컴포넌트 추가
    단순 import 용도의 컴포넌트

---
+++ src/components/atoms/InteractionState/InteractionState.tsx
@@ -0,0 +1,10 @@
+/**
+ * interactionState class를 가지고 있는 div입니다.
+ *
+ * interactionState를 사용하려면 컴포넌트의 Wrapper의 vatiants에 interactionStateVariants를 상속하고
+ *
+ * 해당 컴포넌트를 대상 컴포넌트 Wapper의 바로 아래 최상단에 넣어주세요.
+ */
+const InteractionState = () => <div className="interactionState" />;
+
+export default InteractionState;
