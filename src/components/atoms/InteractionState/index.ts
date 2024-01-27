03522fb6b83245d9cc495dedf10a4c1c1aaa4531
bh2980 <bh2980@naver.com>
2024-01-28 00:20:14 +0900

    chore: 🐋 index.ts 추가

---
+++ src/components/atoms/InteractionState/index.ts
@@ -0,0 +1,6 @@
+import InteractionState from "./InteractionState";
+import { focusVariants, interactionStateVariants } from "./InteractionState.style";
+
+export { interactionStateVariants, focusVariants };
+
+export default InteractionState;
