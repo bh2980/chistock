9a929ca16112aa48e47e4728a7deea3affc01a32
bh2980 <bh2980@naver.com>
2024-02-02 01:37:30 +0900

    refactor: ♻️ convertButtonProps를 useButton으로 수정
    기존 코드를 useStyleVariant, useButtonDefaultProps로 분리

--- src/components/atoms/Button/Button/useButton.ts
+++ src/components/atoms/Button/Button/useButton.ts
@@ -2,7 +2,20 @@ import { Children } from "react";
 
 import type { ButtonAlterAs, ButtonDefault, ButtonProps } from "./Button.types";
 
-const useButton = (props: ButtonProps<ButtonDefault | ButtonAlterAs>) => {
+const useButtonStyleVariant = (props: ButtonProps<ButtonDefault | ButtonAlterAs>) => {
+  const { variant, size, isIconButton, disabled, className } = props;
+
+  return {
+    className,
+    variant,
+    size,
+    disabled,
+    isIconButton,
+    focusOutlineOffset: true,
+  };
+};
+
+const useButtonDefaultProps = (props: ButtonProps<ButtonDefault | ButtonAlterAs>) => {
   const {
     renderAs,
     children,
@@ -21,6 +34,7 @@ const useButton = (props: ButtonProps<ButtonDefault | ButtonAlterAs>) => {
 
   let childrenWithIcon: React.ReactNode | React.ReactNode[] | undefined = undefined;
 
+  // icon이 있으면서 아이콘 버튼이 아닐 경우 icon 위치에 따라 놓기
   if (icon && !isIconButton) {
     if (iconPosition === "before") {
       childrenWithIcon = [icon, ...Children.toArray(children)];
@@ -29,23 +43,32 @@ const useButton = (props: ButtonProps<ButtonDefault | ButtonAlterAs>) => {
     }
   }
 
+  // icon버튼이라면 children 에 icon만 넣기
   if (isIconButton) {
     childrenWithIcon = icon;
   }
+
+  // styleVariants의 className과 중복되지 않도록 제거
+  delete otherProps.className;
 
   return {
     renderAs: renderAs || "button",
     children: icon ? childrenWithIcon : children,
     disabled,
-    isIconButton,
     role: isAnchor ? "button" : role,
     tabIndex: isAnchor && !disabled ? 0 : tabIndex,
-    focusOutlineOffset: true,
     onClick: isAnchor && disabled ? undefined : onClick,
     "aria-label": label,
     "aria-disabled": disabled,
     ...otherProps,
   };
+};
+
+const useButton = (props: ButtonProps<ButtonDefault | ButtonAlterAs>) => {
+  const styleVariant = useButtonStyleVariant(props);
+  const defaultProps = useButtonDefaultProps(props);
+
+  return { ...defaultProps, styleVariant };
 };
 
 export default useButton;
