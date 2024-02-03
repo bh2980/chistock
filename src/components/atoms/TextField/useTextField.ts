d9bcbb203d409a2583ef04e9bc232980ecde9a17
bh2980 <bh2980@naver.com>
2024-02-03 23:35:08 +0900

    refactor: ♻️ uuid 제거 및 useID 적용

--- src/components/atoms/TextField/useTextField.ts
+++ src/components/atoms/TextField/useTextField.ts
@@ -1,21 +1,16 @@
-import { useRef } from "react";
-import { v4 as uuid } from "uuid";
+import useID from "@utils/hook/useID";
 
 import { TextFieldProps } from "./TextField.types";
 
 const useTextFieldProps = (props: TextFieldProps) => {
-  const idRef = useRef(props.id);
+  const inputID = useID("input", props.id);
 
   const { label, placeholder, ...otherProps } = props;
 
   const haveLabel = !!label;
   const tempPlaceholder = placeholder ? placeholder : "";
 
-  if (!idRef.current) {
-    idRef.current = `id-${uuid()}`;
-  }
-
-  return { id: idRef.current, label, haveLabel, placeholder: tempPlaceholder, ...otherProps };
+  return { id: inputID, label, haveLabel, placeholder: tempPlaceholder, ...otherProps };
 };
 
 export default useTextFieldProps;
