6bab12ddd538f34830550d654c7a1ef332b2ce3a
bh2980 <bh2980@naver.com>
2024-02-03 23:30:17 +0900

    test: ✅ ExpandTile 테스트 코드 작성

---
+++ src/components/atoms/Tile/ExpandTile/ExpandTile.test.tsx
@@ -0,0 +1,145 @@
+import { act, render, renderHook, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
+import ExpandTile from "./ExpandTile";
+import useExpandTile from "./useExpandTile";
+
+describe("useExpandTile", () => {
+  it("초기화 테스트", () => {
+    const { result } = renderHook(() => useExpandTile({}));
+    expect(result.current.styleVariant.isExpand).toBe(false);
+  });
+
+  it("상태 토글 테스트", () => {
+    const { result } = renderHook(() => useExpandTile({}));
+    const { buttonProps } = result.current;
+
+    expect(result.current.styleVariant.isExpand).toBe(false);
+
+    act(() => {
+      buttonProps.onClick();
+    });
+
+    expect(result.current.styleVariant.isExpand).toBe(true);
+  });
+
+  it("aria 속성 테스트", () => {
+    const { result } = renderHook(() => useExpandTile({}));
+    const { buttonProps } = result.current;
+
+    expect(result.current.buttonProps["aria-expanded"]).toBe(false);
+    act(() => {
+      buttonProps.onClick();
+    });
+    expect(result.current.buttonProps["aria-expanded"]).toBe(true);
+  });
+
+  it("그라데이션 표시 테스트", () => {
+    const { result } = renderHook(() => useExpandTile({ hideWithGradient: true }));
+    const { buttonProps } = result.current;
+
+    expect(result.current.showGradient).toBe(true);
+    act(() => {
+      buttonProps.onClick();
+    });
+    expect(result.current.showGradient).toBe(false);
+  });
+
+  it("ID 생성 테스트", () => {
+    const { result } = renderHook(() => useExpandTile({}));
+    expect(result.current.id).toContain("expand-tile");
+  });
+
+  it("사용자 제공 ID 테스트", () => {
+    const { result } = renderHook(() => useExpandTile({ id: "custom-id" }));
+    expect(result.current.id).toBe("custom-id");
+  });
+});
+
+describe("ExpandTile", () => {
+  it("렌더링 테스트", () => {
+    render(<ExpandTile />);
+    expect(screen.getByText("더 보기")).toBeInTheDocument();
+  });
+
+  it("본문 렌더링 테스트", () => {
+    render(<ExpandTile>본문</ExpandTile>);
+
+    expect(screen.getByText("본문")).toBeInTheDocument();
+  });
+
+  it("더 보기 버튼 클릭 후 텍스트 변경 테스트", async () => {
+    render(<ExpandTile />);
+
+    const button = screen.getByRole("button");
+    await userEvent.click(button);
+
+    expect(button).toHaveTextContent("닫기");
+  });
+
+  it("ARIA 속성 테스트", async () => {
+    render(<ExpandTile />);
+
+    const button = screen.getByRole("button");
+    expect(button).toHaveAttribute("aria-expanded", "false");
+
+    await userEvent.click(button);
+
+    expect(button).toHaveAttribute("aria-expanded", "true");
+  });
+
+  it("임의 발급 ID 테스트", () => {
+    render(<ExpandTile />);
+
+    const button = screen.getByRole("button");
+    // eslint-disable-next-line testing-library/no-node-access
+    const expandContent = document.getElementById(button.getAttribute("aria-controls")!);
+
+    expect(button).toHaveAttribute("aria-controls", expandContent!.id);
+  });
+
+  it("버튼을 누른 후 ID 유지 확인", async () => {
+    render(<ExpandTile />);
+
+    const button = screen.getByRole("button");
+    // eslint-disable-next-line testing-library/no-node-access
+    const expandContent = document.getElementById(button.getAttribute("aria-controls")!);
+    const initialId = expandContent?.id;
+
+    await userEvent.click(button);
+
+    expect(expandContent).toBeInTheDocument();
+    expect(expandContent?.id).toBe(initialId);
+  });
+
+  it("사용자 지정 ID 테스트", async () => {
+    render(<ExpandTile id="custom-expand-tile" />);
+
+    const button = screen.getByRole("button");
+
+    expect(button).toHaveAttribute("aria-controls", "custom-expand-tile");
+  });
+
+  it("사용자 정의 closeText 및 expandText 테스트", async () => {
+    render(<ExpandTile closeText="닫기 버튼" expandText="열기 버튼" />);
+
+    const button = screen.getByRole("button");
+    expect(screen.getByText("열기 버튼")).toBeInTheDocument();
+    await userEvent.click(button);
+
+    expect(screen.getByText("닫기 버튼")).toBeInTheDocument();
+  });
+
+  it("초기 상태가 확장된 상태로 시작하는지 테스트", () => {
+    render(<ExpandTile expanded />);
+    expect(screen.getByText("닫기")).toBeInTheDocument();
+  });
+
+  it("다른 props가 올바르게 전달되는지 테스트", () => {
+    render(<ExpandTile data-testid="custom-test-id" className="custom-class" />);
+
+    const component = screen.getByTestId("custom-test-id");
+
+    expect(component).toHaveClass("custom-class");
+  });
+});
