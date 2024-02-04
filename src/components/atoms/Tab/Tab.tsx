c4d6931ea9a7c871212f284d7c18a60c65e27341
bh2980 <bh2980@naver.com>
2024-02-05 01:15:14 +0900

    feat: ✨ tab 환현 순회 기능 추가

--- src/components/atoms/Tab/Tab.tsx
+++ src/components/atoms/Tab/Tab.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 //TabList : Tab을 담는 컨테이너
 //Tab : 라벨을 가진 버튼
 //TabPanel : Tab을 클릭 시 활성화되는 영역
@@ -5,6 +6,8 @@
 // TODO 접근성 패턴 읽고 FocusTrap 만들기
 // TODO unselect 불가 리스트 만들기
 import { SelectedListProvider } from "@contexts/SelectedContext";
+import { useRef } from "react";
+import React from "react";
 import { VariantProps } from "tailwind-variants";
 
 import { ComponentPropsWithInnerRef } from "@customTypes/utilType";
@@ -22,10 +25,53 @@ export type TabListProps = ComponentPropsWithInnerRef<"div"> & {
   defaultSelected?: string;
 };
 
-const TabList = ({ defaultSelected, ...props }: TabListProps) => {
+const TabList = ({ defaultSelected, children, ...props }: TabListProps) => {
+  // // Tab해서 Tab을 focus하고 다시 Tab 할 경우 나가야함.
+  // // Tab이 focus되어있을 경우 좌우 방향키로 Tab focus를 이동시킬 수 있어야함. 클리어
+  // // Tab의 focus는 반복순회 가능해야함 클리어
+  // 기존 onKeyUp 실행
+
+  const tabRefs = useRef<HTMLElement[]>([]);
+
+  const childrenCount = React.Children.count(children);
+
+  const handleTabKeyDown = (
+    index: number,
+    event: React.KeyboardEvent,
+    onKeyUp: ((event: React.KeyboardEvent) => void) | undefined = undefined
+  ) => {
+    if (event.key === "ArrowLeft") {
+      const prevTabIndex = (index - 1 + childrenCount) % childrenCount;
+      tabRefs.current[prevTabIndex].focus();
+    } else if (event.key === "ArrowRight") {
+      const nextTabIndex = (index + 1) % childrenCount;
+      tabRefs.current[nextTabIndex].focus();
+    }
+
+    if (onKeyUp) onKeyUp(event);
+  };
+
   return (
     <SelectedListProvider defaultSelected={defaultSelected}>
-      <Slot className="flex gap-s" role="tablist" aria-orientation="horizontal" {...props} />
+      <Slot className="flex gap-s" role="tablist" aria-orientation="horizontal" {...props}>
+        {React.Children.map(children, (child, index) => {
+          if (!React.isValidElement(child) || child.type !== Tab) return null;
+
+          const { onKeyUp } = child.props;
+
+          const saveTabRefs = (el: HTMLElement | null) => {
+            if (el) tabRefs.current[index] = el;
+          };
+
+          const newProps = {
+            ...child.props,
+            innerRef: saveTabRefs,
+            onKeyUp: (event: React.KeyboardEvent) => handleTabKeyDown(index, event, onKeyUp),
+          };
+
+          return React.cloneElement(child, newProps);
+        })}
+      </Slot>
     </SelectedListProvider>
   );
 };
