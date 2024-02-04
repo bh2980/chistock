1b5f84d643ee889ef0aea0ecc845305c271b34fe
bh2980 <bh2980@naver.com>
2024-02-04 14:56:13 +0900

    test: ✅ Toggle Button 테스트 코드에서 hook 부분 삭제

--- src/components/atoms/Button/ToggleButton/ToggleButton.test.tsx
+++ src/components/atoms/Button/ToggleButton/ToggleButton.test.tsx
@@ -1,86 +1,57 @@
-import { render, renderHook, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 
 import ToggleButton from ".";
-import useToggleButton from "./useToggleButton";
 
 describe("ToggleButton", () => {
-  describe("useToggleButton", () => {
-    it("아무 값도 주어지지 않을 경우 aria-pressed가 false여야합니다.", () => {
-      const { result } = renderHook(() => useToggleButton({}));
+  it("에러 없이 정상적으로 렌더링되어야 합니다.", () => {
+    render(<ToggleButton>버튼</ToggleButton>);
 
-      expect(result.current["aria-pressed"]).toBe(false);
-    });
+    const button = screen.getByRole("button");
 
-    it("pressed가 false로 주어질 경우 aria-pressed가 false여야합니다.", () => {
-      const { result } = renderHook(() => useToggleButton({ pressed: false }));
-
-      expect(result.current["aria-pressed"]).toBe(false);
-    });
-
-    it("pressed가 true로 주어질 경우 aria-pressed가 true여야합니다.", () => {
-      const { result } = renderHook(() => useToggleButton({ pressed: true }));
-
-      expect(result.current["aria-pressed"]).toBe(true);
-    });
-
-    it("pressed의 값이 styleVariant에 들어있어야 합니다.", () => {
-      const { result } = renderHook(() => useToggleButton({ pressed: false }));
-
-      expect(result.current.styleVariant).toHaveProperty("pressed", false);
-    });
+    expect(button.tagName).toBe("BUTTON");
   });
 
-  describe("Toggle Button 컴포넌트", () => {
-    it("에러 없이 정상적으로 렌더링되어야 합니다.", () => {
-      render(<ToggleButton>버튼</ToggleButton>);
+  it("기본값으로 aria-pressed가 false여야합니다.", () => {
+    render(<ToggleButton>버튼</ToggleButton>);
 
-      const button = screen.getByRole("button");
+    const button = screen.getByRole("button");
 
-      expect(button.tagName).toBe("BUTTON");
-    });
+    expect(button).toHaveAttribute("aria-pressed", "false");
+  });
 
-    it("기본값으로 aria-pressed가 false여야합니다.", () => {
-      render(<ToggleButton>버튼</ToggleButton>);
+  it("pressed를 true로 줄 경우 aria-pressed가 true여야합니다..", () => {
+    render(<ToggleButton pressed={true}>버튼</ToggleButton>);
 
-      const button = screen.getByRole("button");
+    const button = screen.getByRole("button");
 
-      expect(button).toHaveAttribute("aria-pressed", "false");
-    });
+    expect(button).toHaveAttribute("aria-pressed", "true");
+  });
 
-    it("pressed를 true로 줄 경우 aria-pressed가 true여야합니다..", () => {
-      render(<ToggleButton pressed={true}>버튼</ToggleButton>);
+  it("버튼을 click 시 aria-pressed가 true로 변경되어야합니다.", async () => {
+    const onClick = jest.fn();
+    render(<ToggleButton onClick={onClick}>버튼</ToggleButton>);
 
-      const button = screen.getByRole("button");
+    const button = screen.getByRole("button");
 
-      expect(button).toHaveAttribute("aria-pressed", "true");
-    });
+    expect(button).toHaveAttribute("aria-pressed", "false");
 
-    it("버튼을 click 시 aria-pressed가 true로 변경되어야합니다.", async () => {
-      const onClick = jest.fn();
-      render(<ToggleButton onClick={onClick}>버튼</ToggleButton>);
+    await userEvent.click(button);
 
-      const button = screen.getByRole("button");
+    expect(button).toHaveAttribute("aria-pressed", "true");
+  });
 
-      expect(button).toHaveAttribute("aria-pressed", "false");
+  it("버튼을 click 후 다시 click 시 aria-pressed가 false로 변경되어야합니다.", async () => {
+    const onClick = jest.fn();
+    render(<ToggleButton onClick={onClick}>버튼</ToggleButton>);
 
-      await userEvent.click(button);
+    const button = screen.getByRole("button");
 
-      expect(button).toHaveAttribute("aria-pressed", "true");
-    });
+    expect(button).toHaveAttribute("aria-pressed", "false");
 
-    it("버튼을 click 후 다시 click 시 aria-pressed가 false로 변경되어야합니다.", async () => {
-      const onClick = jest.fn();
-      render(<ToggleButton onClick={onClick}>버튼</ToggleButton>);
+    await userEvent.click(button);
+    await userEvent.click(button);
 
-      const button = screen.getByRole("button");
-
-      expect(button).toHaveAttribute("aria-pressed", "false");
-
-      await userEvent.click(button);
-      await userEvent.click(button);
-
-      expect(button).toHaveAttribute("aria-pressed", "false");
-    });
+    expect(button).toHaveAttribute("aria-pressed", "false");
   });
 });
