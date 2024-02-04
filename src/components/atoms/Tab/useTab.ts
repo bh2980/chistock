11a659f8aa0eea60e541a0a09e02640cc11b0341
bh2980 <bh2980@naver.com>
2024-02-05 00:03:28 +0900

    feat: ✨ selected 탭을 다시 클릭 시 해제 불가능하도록 수정

--- src/components/atoms/Tab/useTab.ts
+++ src/components/atoms/Tab/useTab.ts
@@ -7,11 +7,15 @@ import { TabProps } from "./Tab";
 const useTab = (props: TabProps) => {
   const buttonProps = useButton(props as ButtonProps<"button">);
 
-  const { styleVariant, ...selectedProps } = useSelectedList(buttonProps);
+  const { styleVariant, onClick, ...selectedProps } = useSelectedList(buttonProps);
+  const ariaSelected = selectedProps["aria-selected"];
+
+  const uncancelableOnClick = ariaSelected ? undefined : onClick;
 
   return {
     styleVariant: { ...styleVariant, focusOutlineOffset: false },
     ...selectedProps,
+    onClick: uncancelableOnClick,
     role: "tab",
   };
 };
