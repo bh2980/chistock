38f36308ebcf3a36dfd06a6ec3e479ba6e89c2ce
bh2980 <bh2980@naver.com>
2024-02-05 00:01:31 +0900

    chore: 🐋 TabListProps-defaultSelected에서 string[] 삭제

--- src/components/atoms/Tab/Tab.tsx
+++ src/components/atoms/Tab/Tab.tsx
@@ -1,6 +1,9 @@
 //TabList : Tab을 담는 컨테이너
 //Tab : 라벨을 가진 버튼
 //TabPanel : Tab을 클릭 시 활성화되는 영역
+// TODO Tab Panel 만들기 -> 조건부 렌더 컴포넌트 만들면 될 듯
+// TODO 접근성 패턴 읽고 FocusTrap 만들기
+// TODO unselect 불가 리스트 만들기
 import { SelectedListProvider } from "@contexts/SelectedContext";
 import { VariantProps } from "tailwind-variants";
 
@@ -16,7 +19,7 @@ export type TabProps = ComponentPropsWithInnerRef<"button"> &
   VariantProps<typeof tabVariant> & { value: string };
 
 export type TabListProps = ComponentPropsWithInnerRef<"div"> & {
-  defaultSelected?: string | string[];
+  defaultSelected?: string;
 };
 
 const TabList = ({ defaultSelected, ...props }: TabListProps) => {
