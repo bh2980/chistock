b50b2bc547db8b517be66c636ff186f7d64a0d00
bh2980 <bh2980@naver.com>
2024-02-06 14:58:24 +0900

    refactor: ♻️ Label을 Slot으로 변경

--- src/components/atoms/Label/Label.tsx
+++ src/components/atoms/Label/Label.tsx
@@ -1,11 +1,13 @@
+import Slot from "@atoms/Slot/Slot";
+
 import { labelVariants } from "./Label.styles";
 import type { LabelProps } from "./Label.types";
 
 const Label = ({ children, variant, size, className, ...props }: LabelProps) => {
   return (
-    <span className={labelVariants({ variant, size, className })} {...props}>
+    <Slot renderAs="span" className={labelVariants({ variant, size, className })} {...props}>
       {children}
-    </span>
+    </Slot>
   );
 };
 
