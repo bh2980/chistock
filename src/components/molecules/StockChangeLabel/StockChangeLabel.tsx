3745a6648ed92c5660fdd310a3f85b861be746f1
bh2980 <bh2980@naver.com>
2024-02-06 16:39:54 +0900

    refactor: ♻️ 증가 시 빨간색으로 변경
    showPercentage로 이름 변경 및 템플릿 리터럴 설정

--- src/components/molecules/StockChangeLabel/StockChangeLabel.tsx
+++ src/components/molecules/StockChangeLabel/StockChangeLabel.tsx
@@ -11,18 +11,18 @@ type StockChangeLabelProps = { change: number; changePercentage: number } & Omit
 // 증감값과 증감률을 번갈아가며 보여준다.
 const StockChangeLabel = ({ change, changePercentage, ...labelProps }: StockChangeLabelProps) => {
   const isPositive = change > 0;
-  const labelVariant = isPositive ? "primary" : "error";
+  const labelVariant = isPositive ? "error" : "primary";
 
   const [content, setContent] = useState(change);
-  const [isPercentage, setIsPercentage] = useState(false);
+  const [showPercentage, setShowPercentage] = useState(false);
 
   useEffect(() => {
-    setContent(isPercentage ? changePercentage : change);
-  }, [isPercentage, changePercentage, change]);
+    setContent(showPercentage ? changePercentage : change);
+  }, [showPercentage, changePercentage, change]);
 
   useEffect(() => {
     const intervalId = setInterval(
-      (resolve) => resolve(setIsPercentage((current) => !current)),
+      (resolve) => resolve(setShowPercentage((current) => !current)),
       5000
     );
 
@@ -32,11 +32,13 @@ const StockChangeLabel = ({ change, changePercentage, ...labelProps }: StockChan
   }, []);
 
   return (
-    <Label variant={labelVariant} {...labelProps}>
-      <div className="animate-fadeInOut text-center">{content}</div>
+    <Label variant={labelVariant} className="overflow-hidden" {...labelProps}>
+      <div className="animate-fadeInOut text-center">{`${isPositive ? "▲" : "▼"} ${content}${
+        showPercentage ? "%" : ""
+      }`}</div>
       <div className="flex flex-col invisible">
-        <div>{change}</div>
-        <div>{changePercentage}</div>
+        <div>▲ {change}</div>
+        <div>▲ {changePercentage} %</div>
       </div>
     </Label>
   );
