8cf1d38a888a1ecc25f92a42f79310e3f2df2501
bh2980 <bh2980@naver.com>
2024-02-08 21:20:25 +0900

    fix: 🐛 테스트 코드 변경

--- src/components/molecules/StockPrice/__test__/StockPrice.test.tsx
+++ src/components/molecules/StockPrice/__test__/StockPrice.test.tsx
@@ -5,9 +5,17 @@ import StockPrice from "../StockPrice";
 
 describe("StockPrice", () => {
   it("prefix, postfix를 렌더링해야합니다.", () => {
-    render(<StockPrice price={1234.5678} prefix="$" postfix=" USD" decimalPoint={2} />);
+    render(
+      <StockPrice
+        price={1234.5678}
+        prefix="$"
+        postfix=" USD"
+        decimalPoint={2}
+        data-testid="stock-price"
+      />
+    );
 
-    expect(screen.getByText("$")).toBeInTheDocument();
-    expect(screen.getByText(" USD")).toBeInTheDocument();
+    expect(screen.getByTestId("stock-price")).toHaveTextContent("$");
+    expect(screen.getByTestId("stock-price")).toHaveTextContent(" USD");
   });
 });
